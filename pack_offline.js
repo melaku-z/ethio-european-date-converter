f11cc23e4ec0812ed222635b5bdd7884358c4ce2
melaku.zewdu <melaku44@gmail.com>
2018-08-26 18:25:11 +0300

    untested

--- pack_offline.js
+++ pack_offline.js
@@ -1,13 +1,27 @@
 //pb is package-bundle
 const nodeLocation = 'C:/Users/user/AppData/Roaming/npm/node_modules/'
 const pb = require(nodeLocation  + 'package-bundle/bin/package-bundle.js')
-var args = ["bootstrap", "jquery"]
-var options = {
-  // F: "C:/Users/user/AppData/Roaming/npm/pack1.zip"
+const args = ["bootstrap", "jquery"]
+const options = {
+  F: "C:/Users/user/AppData/Roaming/npm/pack1.zip"
 }
-process.argv = args
-pb()
+
 // cmd
 // package-bundle request bluebird -F
 // -F or --out-file <file> 
 // npm cache add <folder>
+
+const { spawn } = require('child_process');
+const packageBundleBat = spawn('cmd.exe', ['/c', 'package-bundle', args, options]);
+
+packageBundleBat.stdout.on('data', (data) => {
+  console.log(data.toString());
+});
+
+packageBundleBat.stderr.on('data', (data) => {
+  console.log(data.toString());
+});
+
+packageBundleBat.on('exit', (code) => {
+  console.log(`Child exited with code ${code}`);
+});
