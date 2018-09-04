42b984437a90173c4669fa6151e2d76c96aa3fac
melaku.zewdu <melaku44@gmail.com>
2018-09-04 19:12:49 +0300

    dateconverter's initDates work, but no jquery's $

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 
 import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/css/bootstrap.css'
-import './js/dateconverter.js'
 import './css/dateconverter.css'
+import './js/dateconverter.js'
+const $ = require('jquery') //todo: remove from global
