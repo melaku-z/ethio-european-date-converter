6b8d04d8d97e3a4a5a79297b50e38f4db54d9ec6
melaku.zewdu <melaku44@gmail.com>
2018-09-05 21:25:18 +0300

    untested

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import './css/dateconverter.css'
-import './js/dateconverterUI.js'
-const dateconverterUI = require('./js/dateconverterUI.js')
+import * as dateconverterUI from './js/dateconverterUI.js'
 dateconverterUI.createEventListnersHTML()
