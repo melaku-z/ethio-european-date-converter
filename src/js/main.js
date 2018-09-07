48d828a4c9153a1d05b4a3e20a6bfbb1267b68ab
melaku.zewdu <melaku44@gmail.com>
2018-09-07 13:27:05 +0300

    refactor working

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,2 @@
-const dateconverterUI = require('./dateconverterUI')
-
-function createEventListnersHTML(){
-  dateconverterUI.$('body').onload=dateconverterUI.initDates
-  document.querySelector('#refreshEthDateButton').onclick=dateconverterUI.refreshEthDateOnPage
-  document.querySelector('#EuropeanDate').onchange=dateconverterUI.updateCalculatedEthDateOnPage
-  document.querySelector('#EthMonthScroll').onchange=dateconverterUI.updateCalculatedEurDateOnPage
-  document.querySelector('#EthDayScroll').onchange=dateconverterUI.updateCalculatedEurDateOnPage
-  document.querySelector('#EthYearScroll').onchange=dateconverterUI.updateCalculatedEurDateOnPage
-  dateconverterUI.initDates()
-}
+import {createEventListnersHTML} from './dateconverterUI'
 createEventListnersHTML()
-
-module.exports = {
-  initDates: dateconverterUI.initDates,
-  refreshEthDateOnPage: dateconverterUI.refreshEthDateOnPage,
-  updateCalculatedEthDateOnPage: dateconverterUI.updateCalculatedEthDateOnPage,
-  updateCalculatedEurDateOnPage: dateconverterUI.updateCalculatedEurDateOnPage,
-  $: dateconverterUI.$
-}
