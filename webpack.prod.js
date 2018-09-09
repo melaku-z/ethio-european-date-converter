3009a772b23d412c103fea58848b05d86edf1c96
melaku.zewdu <melaku44@gmail.com>
2018-09-09 22:37:56 +0300

    js split to vendors

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,22 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new CleanWebpackPlugin(['public_html_temp'])
-  ]
-});
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
+})
