f15d3bf45b4c6428e92963e113e3b22caac0d01d
melaku.zewdu <melaku44@gmail.com>
2018-09-09 22:39:02 +0300

    minor

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,9 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './public_html_temp'
-  }
-});
+  },
+})
