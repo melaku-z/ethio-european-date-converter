ed91a57d6283dc79f1a6336b457507d6a1e93601
melaku.zewdu <melaku44@gmail.com>
2018-09-15 19:10:55 +0300

    refactor

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,9 @@
 const path = require('path')
+const devMode = process.env.NODE_ENV !== 'production'
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const WorkboxPlugin = require('workbox-webpack-plugin')
 const pathToMainJs = require.resolve('./src/js/main.js')
 const pathToIndexHtml = require.resolve('./src/index.html')
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
-const AppManifestWebpack = require('app-manifest-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const PurifyCSSPlugin = require('purifycss-webpack')
 module.exports = {
@@ -20,43 +19,7 @@ module.exports = {
   plugins: [
     new FriendlyErrorsWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'css/[name][contenthash].css'}
-    ),
-    new HtmlWebpackPlugin({
-      template: pathToIndexHtml,
-      minify: true
-    }),
-    new AppManifestWebpack({
-      logo: './src/img/icons.png',
-      output: '/assets/icons-[hash:8]/',
-      config: {
-        appName: 'Ethio-European Date Converter',
-        appDescription: 'Convert dates between Ethiopian and European calendar offline.',
-        developerName: 'Melaku Zewdu',            // Your (or your developer's) name. `string`
-        developerURL: 'github.com/melaku-z',             // Your (or your developer's) URL. `string`
-        background: '#fff',             // Background colour for flattened icons. `string`
-        theme_color: '#157878',         // Theme color for browser chrome. `string`
-        display: 'standalone',          // Android display: "browser" or "standalone". `string`
-        orientation: 'portrait',        // Android orientation: "portrait" or "landscape". `string`
-        start_url: '/?homescreen=1',    // Android start application's URL. `string`
-        version: '1.1',
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          firefox: false,
-          yandex: false,
-          windows: true
-        }
-      }
-    }),
-    new WorkboxPlugin.GenerateSW({
-      importWorkboxFrom: 'local',
-      importsDirectory: '/assets/sw/',
-      exclude: [/(\.(txt|htaccess|png|xml)$)|(404.html$)|(favicon.ico$)/],
-      offlineGoogleAnalytics: true
+      filename: 'css/[name].[contenthash].css',
     }),
     new PurifyCSSPlugin({
       paths: [
@@ -67,7 +30,12 @@ module.exports = {
       purifyOptions: {
         whitelist: []
       }
-    })
+    }),
+    new HtmlWebpackPlugin({
+      template: pathToIndexHtml,
+      minify: true,
+      inject: 'body'
+    }),
   ],
   output: {
     filename: './js/[name].[chunkhash].js',
@@ -93,8 +61,8 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [          
-          MiniCssExtractPlugin.loader,
+        use: [      
+          MiniCssExtractPlugin.loader,  
           'css-loader'
         ]
       }, 
@@ -122,7 +90,6 @@ module.exports = {
           }
         ]
       },
-      // file-loader(for fonts)
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,12 +5,46 @@ const WriteFilePlugin = require('write-file-webpack-plugin')
 const path = require('path')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
+const AppManifestWebpack = require('app-manifest-webpack-plugin')
 
 module.exports = merge.smart(common, {
   mode: 'production',
   plugins: [
     new CleanWebpackPlugin(['public_html']),
-    new WriteFilePlugin()
+    new WriteFilePlugin(),
+    new AppManifestWebpack({
+      logo: './src/img/icons.png',
+      output: '/assets/icons-[hash:8]/',
+      config: {
+        appName: 'Ethio-European Date Converter',
+        appDescription: 'Convert dates between Ethiopian and European calendar offline.',
+        developerName: 'Melaku Zewdu',            // Your (or your developer's) name. `string`
+        developerURL: 'github.com/melaku-z',             // Your (or your developer's) URL. `string`
+        background: '#fff',             // Background colour for flattened icons. `string`
+        theme_color: '#157878',         // Theme color for browser chrome. `string`
+        display: 'standalone',          // Android display: "browser" or "standalone". `string`
+        orientation: 'portrait',        // Android orientation: "portrait" or "landscape". `string`
+        start_url: '/?homescreen=1',    // Android start application's URL. `string`
+        version: '1.1',
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: false,
+          yandex: false,
+          windows: true
+        }
+      }
+    }),
+    new WorkboxPlugin.GenerateSW({
+      importWorkboxFrom: 'local',
+      importsDirectory: '/assets/sw/',
+      exclude: [/(\.(txt|htaccess|png|xml)$)|(404.html$)|(favicon.ico$)/],
+      offlineGoogleAnalytics: true
+    })
   ],
   optimization: {
     runtimeChunk: 'single',
