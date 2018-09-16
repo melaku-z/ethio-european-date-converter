86a6434a45ec76110e6a995a3096738c3418305a
melaku.zewdu <melaku44@gmail.com>
2018-09-16 11:26:14 +0300

    jquery reliance replaced by native

--- src/js/dateconverterUI.js
+++ src/js/dateconverterUI.js
@@ -1,20 +1,19 @@
 'use strict'
-const $ = require('jquery')
 import * as dateConverter from './dateconverter.js'
 
 function updateCalculatedEthDateOnPage() {
-  const ethDate = dateConverter.toEthiopianDateTime(new Date($('#EuropeanDate').val()))
-  $('#EthDayScroll').val(ethDate.date)
-  $('#EthMonthScroll').val(ethDate.month)
-  $('#EthYearScroll').val(ethDate.year)
-  $('#EurDayTextArea').html(dateConverter.toEuropeanDateString(ethDate))
-  $('#ethDayTextArea').html(ethDate.dateWithDayString)
+  const ethDate = dateConverter.toEthiopianDateTime(new Date(document.getElementById('EuropeanDate').val()))
+  document.getElementById('EthDayScroll').val(ethDate.date)
+  document.getElementById('EthMonthScroll').val(ethDate.month)
+  document.getElementById('EthYearScroll').val(ethDate.year)
+  document.getElementById('EurDayTextArea').html(dateConverter.toEuropeanDateString(ethDate))
+  document.getElementById('ethDayTextArea').html(ethDate.dateWithDayString)
 }
 
 function updateCalculatedEurDateOnPage() {
-  const ethDate_Year = parseInt($('#EthYearScroll')[0].value)
-  const ethDate_Month = parseInt($('#EthMonthScroll')[0].value)
-  const ethDate_Day = parseInt($('#EthDayScroll')[0].value)
+  const ethDate_Year = parseInt(document.getElementById('EthYearScroll')[0].value)
+  const ethDate_Month = parseInt(document.getElementById('EthMonthScroll')[0].value)
+  const ethDate_Day = parseInt(document.getElementById('EthDayScroll')[0].value)
   const ethDate = new dateConverter.ethTime(ethDate_Day, ethDate_Month, ethDate_Year, 0, 0, 0)
   const eurDateTime = dateConverter.toEuropeanDate(ethDate)
   var eurDate
@@ -22,18 +21,18 @@ function updateCalculatedEurDateOnPage() {
     eurDate = eurDateTime.toJSON().slice(0, 10)
   else
     eurDate = eurDateTime
-  $('#EuropeanDate').val(eurDate)
-  $('#EurDayTextArea').html(dateConverter.toEuropeanDateString(ethDate))
-  $('#ethDayTextArea').html(ethDate.dateWithDayString)
+  document.getElementById('EuropeanDate').val(eurDate)
+  document.getElementById('EurDayTextArea').html(dateConverter.toEuropeanDateString(ethDate))
+  document.getElementById('ethDayTextArea').html(ethDate.dateWithDayString)
 }
 
 function refreshEthDateOnPage(){
-  $('#ethTodayTextArea').html(dateConverter.toEthiopianDateTimeString(new Date()))
+  document.getElementById('ethTodayTextArea').html(dateConverter.toEthiopianDateTimeString(new Date()))
 }
 
 function initDates() {
   refreshEthDateOnPage()
-  $('#EuropeanDate').val(new Date().toJSON().slice(0, 10))
+  document.getElementById('EuropeanDate').val(new Date().toJSON().slice(0, 10))
   updateCalculatedEthDateOnPage()
   updateCalculatedEurDateOnPage()
 }
