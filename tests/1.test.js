32e85e42c56eaf60521d5659450109f0aaacfd74
melaku.zewdu <melaku44@gmail.com>
2018-10-17 18:35:10 +0300

    minor

--- tests/1.test.js
+++ tests/1.test.js
@@ -2,7 +2,7 @@ describe('Ethiopian', () => {
     beforeAll(async () => {
         await page.goto('http://localhost:8080/').catch(reason => {
             console.log(reason);
-        })
+        });
     });
 
     it('should display "Ethiopian" text on page', async () => {
