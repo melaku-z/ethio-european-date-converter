cc541eb09f6e583b3dd0e4e1835499e6e60b15b2
melaku.zewdu <melaku44@gmail.com>
2018-10-20 13:20:15 +0300

    date mocked in weird way, tests passing

--- tests/1.test.js
+++ tests/1.test.js
@@ -30,11 +30,20 @@ describe('Ethiopian', () => {
 
         it('should show expected date', async () => {
             if (!browserFailed) {
-                mockDate('2018-10-20 22:23:56 GMT+0300');
+                await page.evaluate((isoDate) => {
+                    const RealDate = Date;
+                    Date = class extends RealDate {
+                        constructor(...args) {
+                            if (args.length) return new RealDate(...args);
+                            return new RealDate(isoDate);
+                        }
+                    };
+                    onload();
+                }, '2018-10-17 22:23:56 GMT+0300');
                 await expect(page).toMatch('Ethiopian Calendar is Tikimt');
-                await expect(page).toMatch('Ethiopian Calendar is Tikimt 9, 2011');
-                // await expect(page).toMatch('Sat, 20 Oct 2018 (at GMT+0)');
-                // await expect(page).toMatch('Saturday, Tikimt 10, 2011');
+                await expect(page).toMatch('Ethiopian Calendar is Tikimt 7, 2011');
+                await expect(page).toMatch('Wed, 17 Oct 2018 (at GMT+0)');
+                await expect(page).toMatch('Wednesday, Tikimt 7, 2011');
             }
         });
     }
