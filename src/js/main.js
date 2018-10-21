e04cc03437a1710425bf5266f615ad5f64389232
melaku.zewdu <melaku44@gmail.com>
2018-10-21 19:47:11 +0300

    date update button works now

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,6 @@
 require('bootstrap/dist/css/bootstrap.css');
 require('../css/dateconverter.css');
-var Vue = require('vue/dist/vue.min.js');
 // require('@fortawesome/fontawesome-free/js/all.js');
 // require('@fortawesome/fontawesome-free/css/brands.css');
-import { createEventListnersHTML, toEthiopianDateTimeString } from './dateconverterUI.js';
+import { createEventListnersHTML } from './dateconverterUI.js';
 createEventListnersHTML();
-var ethTodayTextArea = new Vue({
-  el: '#ethTodayTextArea',
-  data: {
-    ethTodayText: toEthiopianDateTimeString(new Date())
-  }
-});
