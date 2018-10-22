6965a3ba3cb4857c7a3723180e47ce9df3338070
melaku.zewdu <melaku44@gmail.com>
2018-10-22 18:59:45 +0300

    build with vue

--- dist/assets/sw/precache-manifest.eddda5b7a7cd9cdb6d5ddcf5fa81f89d.js
+++ dist/assets/sw/precache-manifest.eddda5b7a7cd9cdb6d5ddcf5fa81f89d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = [
   {
-    "revision": "baee90c916a26d7c7013990d898579b7",
+    "revision": "4d129f7ad623cea90292a1af17f59a6e",
     "url": "index.html"
   },
   {
@@ -8,12 +8,12 @@ self.__precacheManifest = [
     "url": "google4ea6bc293950abe1.html"
   },
   {
-    "revision": "fbb03f2ad9f654d48d85",
-    "url": "css/vendors.45c8aeb9600a7958928e.css"
+    "revision": "822ab5d2bba998a65a04",
+    "url": "css/vendors.bc817a7cf87854f91147.css"
   },
   {
-    "revision": "f0daf84ea58329d2bd13",
-    "url": "css/main.499694627553f1b25df6.css"
+    "revision": "945f00cd56cc6dd3fec3",
+    "url": "css/main.ee680c34fe2eb18a0af7.css"
   },
   {
     "revision": "04974f103a94b33dd389085c13c40f99",
@@ -24,12 +24,12 @@ self.__precacheManifest = [
     "url": ".cache"
   },
   {
-    "url": "./js/vendors.fbb03f2ad9f654d48d85.js"
+    "url": "./js/vendors.822ab5d2bba998a65a04.js"
   },
   {
     "url": "./js/runtime.a9e4b6dd872f64a1f107.js"
   },
   {
-    "url": "./js/main.f0daf84ea58329d2bd13.js"
+    "url": "./js/main.945f00cd56cc6dd3fec3.js"
   }
 ];
