cb9f65da506739b1dedb1685b70a26a22c01cffd
melaku.zewdu <melaku44@gmail.com>
2018-10-24 20:38:58 +0300

    migrate to Vue, Eur to Eth working

--- src/js/dateconverterUI.js
+++ src/js/dateconverterUI.js
@@ -43,8 +43,8 @@ function initDates() {
   document.getElementById('EuropeanDate').value = dateAtGMT.toJSON().slice(0, 10);
   updateCalculatedEthDateOnPage();
   updateCalculatedEurDateOnPage();
-  window.onfocus = () => {ethTodayTextArea.liveRefreshEnabled = true;};
-  window.onblur = () => {ethTodayTextArea.liveRefreshEnabled = false;};
+  window.onfocus = () => { ethTodayTextArea.liveRefreshEnabled = true; };
+  window.onblur = () => { ethTodayTextArea.liveRefreshEnabled = false; };
 }
 
 function createEventListnersHTML() {
@@ -65,8 +65,7 @@ var ethTodayTextArea = new Vue({
   },
   methods: {
     refreshEthDateOnPage: function () {
-      [this.ethTodayDateText,this.ethTodayTimeText] = toEthiopianDateTimeString(new Date());
-      // console.log(new Date());
+      [this.ethTodayDateText, this.ethTodayTimeText] = toEthiopianDateTimeString(new Date());
     },
     liveDateRefresh: function () {
       this.liveRefreshObj = setInterval(this.refreshEthDateOnPage, 1000);
@@ -86,7 +85,59 @@ var ethTodayTextArea = new Vue({
   },
 });
 
+var CalendarConverter = new Vue({
+  el: '#CalendarConverter',
+  data: {
+    ethCalDate: '',
+    ethCalMon: '',
+    ethCalYear: '',
+    eurCalForm: '',
+  },
+  computed: {
+    eurCal: function () {
+      const EuropeanDateValueArray = this.eurCalForm.split('-');
+      const eurYear = EuropeanDateValueArray[0];
+      const eurMon = EuropeanDateValueArray[1];
+      const eurDate = EuropeanDateValueArray[2];
+      return new Date(Date.UTC(eurYear, eurMon - 1, eurDate));
+    },
+    ethCalObj: function () {
+      return new ethTime(this.ethCalDate, this.ethCalMon, this.ethCalYear);
+    },
+    ethCalText: function () {
+      return this.ethCalObj.dateWithDayString;
+    },
+    eurCalText: function () {
+      return this.eurCal.toUTCString().substring(0, 16) + ' (at GMT+0)';
+    },
+  },
+  methods: {
+    updateCalculatedEthDate: function () {
+      const calculatedEthDate = toEthiopianDateTime(this.eurCal);
+      [this.ethCalDate, this.ethCalMon, this.ethCalYear] = 
+        [calculatedEthDate.date, calculatedEthDate.month, calculatedEthDate.year];
+    },
+    updateCalculatedEurDate: function () {
+      this.eurCalForm = toEuropeanDate(this.ethCalObj).toJSON().slice(0, 10);
+    },
+  },
+  watch: {
+    eurCal: function () {
+      this.updateCalculatedEthDate();
+      console.log('updateCalculatedEthDate');
+    },
+    ethCalObj: function () {this.updateCalculatedEurDate();},
+  },
+  created: function () {    
+    const currentDate = new Date();
+    const dateAtGMT = new Date(currentDate.valueOf() + currentDate.getTimezoneOffset() * 60000);
+    this.eurCalForm = dateAtGMT.toJSON().slice(0, 10);
+    this.updateCalculatedEthDate();
+  },
+});
+
 export {
   createEventListnersHTML,
-  ethTodayTextArea
+  ethTodayTextArea,
+  CalendarConverter
 };
