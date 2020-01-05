db8d9e746f2fcbb4f5260fccbadcbf478aeb8249
melaku.zewdu <melaku44@gmail.com>
2020-01-05 11:45:28 +0300

    Added footer Application Info

---
+++ src/js/footer.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+
+var footer = new Vue({
+  el: '#footer-info',
+  data: {
+    isCollapsed: true
+  }
+});
+
+export {
+  footer
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,4 @@
 require('bootstrap/dist/css/bootstrap.css');
 require('../css/dateconverter.css');
-// require('@fortawesome/fontawesome-free/js/all.js');
-// require('@fortawesome/fontawesome-free/css/brands.css');
-import {CalendarConverter, ethTodayTextArea} from './dateconverterUIVue.js';
-// createEventListnersHTML();
+import { CalendarConverter, ethTodayTextArea } from './dateconverterUIVue.js';
+import { footer } from './footer.js';
