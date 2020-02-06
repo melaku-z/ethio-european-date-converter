79329a88d519136a6c382e716a3a555c43a573a0
melaku.zewdu <melaku44@gmail.com>
2020-02-06 17:59:36 +0300

    load html from local file for jest

--- tests/home-page.test.js
+++ tests/home-page.test.js
@@ -38,8 +38,12 @@ describe('Ethiopian', () => {
         });
     }
     else {
-      const openPage = url => window.history.replaceState({}, '', url);
-      const page = window.document.querySelector('html').outerHTML;
+      async function openPage(url) {
+        window.history.replaceState({}, '', url)
+        const html = require('fs').readFileSync('./dist/index.html', { encoding: 'utf-8' })
+        window.document.querySelector('html').innerHTML  = html
+      }
+      const page = () => window.document.querySelector('html').outerHTML;
       const evaluateJS = js => js()
       const RealDate = Date;
 
@@ -48,16 +52,15 @@ describe('Ethiopian', () => {
       });
 
       beforeAll(async () => {
-        return new Promise((resolve, reject) => {
-          openPage(global.JestTestURL);
-          window.onModulesLoaded = () => {
-            resolve()
-          }
+        return new Promise(async (resolve, reject) => {
+          await openPage(global.JestTestURL);
+          window.onModulesLoaded = resolve;
+          window.addEventListener('load', resolve)
         })
       });
 
       it('should display "Ethiopian" text on page', async () => {
-          await expect(page).toMatch('Today in Ethiopian Calendar is');
+          await expect(page()).toMatch('Today in Ethiopian Calendar is');
       });
 
       it('should show expected date', async () => {
@@ -72,12 +75,13 @@ describe('Ethiopian', () => {
               }
               const RealDate = Date;
               mockDate(isoDate, RealDate);
-              window.onload();
+              if (onload) onload();
+              document.getElementById('refreshEthDateButton').click()
           }, '2018-10-17 22:23:56 GMT+0300');
-          await expect(page).toMatch('Ethiopian Calendar is Tikimt');
-          await expect(page).toMatch('Ethiopian Calendar is Tikimt 7, 2011');
-          await expect(page).toMatch('Wed, 17 Oct 2018 (at GMT+0)');
-          await expect(page).toMatch('Wednesday, Tikimt 7, 2011');
+          await expect(page()).toMatch('Ethiopian Calendar is Tikimt');
+          await expect(page()).toMatch('Ethiopian Calendar is Tikimt 7, 2011');
+          await expect(page()).toMatch('Wed, 17 Oct 2018 (at GMT+0)');
+          await expect(page()).toMatch('Wednesday, Tikimt 7, 2011');
       });
     }
 });
