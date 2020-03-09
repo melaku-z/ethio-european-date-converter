513cf41fb7d0bacc8d4a82122ba3eb70a81c57be
melaku.zewdu <melaku44@gmail.com>
2020-03-10 01:12:55 +0300

    improved puppeter test

--- tests/home-page.test.js
+++ tests/home-page.test.js
@@ -1,6 +1,14 @@
 describe('Ethiopian', () => {
   if (global.jestPreset == 'jest-puppeteer') {
-    const openPage = page.goto;
+    const puppeteer = require('puppeteer');
+
+    let page;
+    let browser;
+    async function openPage(url) {
+      browser = await puppeteer.launch({headless: true});
+      page = await browser.newPage();
+      await page.goto(url);
+    }
     const RealDate = Date;
 
     afterEach(() => {
@@ -13,8 +21,13 @@ describe('Ethiopian', () => {
       });
     });
 
+    afterAll(() => {
+      browser.close();
+    });
+
     it('should display "Ethiopian" text on page', async () => {
-      await expect(page).toMatch('Today in Ethiopian Calendar is');
+      const pageContent = await page.content()
+      expect(pageContent).toMatch('Today in Ethiopian Calendar is');
     });
 
     it('should show expected date', async () => {
@@ -29,12 +42,13 @@ describe('Ethiopian', () => {
         }
         const RealDate = Date;
         mockDate(isoDate, RealDate);
-        onload();
+        document.querySelector('body').onload();
       }, '2018-10-17 22:23:56 GMT+0300');
-      await expect(page).toMatch('Ethiopian Calendar is Tikimt');
-      await expect(page).toMatch('Ethiopian Calendar is Tikimt 7, 2011');
-      await expect(page).toMatch('Wed, 17 Oct 2018 (at GMT+0)');
-      await expect(page).toMatch('Wednesday, Tikimt 7, 2011');
+      const pageContent = await page.content()
+      await expect(pageContent).toMatch('Ethiopian Calendar is Tikimt');
+      await expect(pageContent).toMatch('Ethiopian Calendar is Tikimt 7, 2011');
+      await expect(pageContent).toMatch('Wed, 17 Oct 2018 (at GMT+0)');
+      await expect(pageContent).toMatch('Wednesday, Tikimt 7, 2011');
     });
   }
   else {
@@ -52,7 +66,7 @@ describe('Ethiopian', () => {
     });
 
     beforeAll(async () => {
-      return new Promise(async (resolve, reject) => {
+      return new Promise(async (resolve) => {
         window.onModulesLoaded = resolve;
         openPage(global.JestTestURL);
         window.onModulesLoaded = resolve;
@@ -76,7 +90,7 @@ describe('Ethiopian', () => {
         }
         const RealDate = Date;
         mockDate(isoDate, RealDate);
-        if (onload) await onload();
+        document.querySelector('body').onload();
         document.getElementById('refreshEthDateButton').click();
       }, '2018-10-17 22:23:56 GMT+0300');
       await expect(page()).toMatch('Ethiopian Calendar is Tikimt');
