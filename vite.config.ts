89e187f1bd6698eeb0ec738bcc749522f4be1f1a
Melaku <melaku44@gmail.com>
2022-12-04 23:19:16 +0300

    simplify ci

--- vite.config.ts
+++ vite.config.ts
@@ -92,6 +92,6 @@ export default defineConfig({
     },
   },
   test: {
-    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache', 'packages'],
+    exclude: ['node_modules', 'dist', '.idea', '.git', '.cache', 'packages', 'e2e'],
   },
 })
