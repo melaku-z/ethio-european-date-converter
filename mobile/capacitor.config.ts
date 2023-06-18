f83c5d2e66313de199c41bb591724b945184c0be
Melaku <melaku44@gmail.com>
2023-06-18 21:01:20 +0300

    refactor android

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli'
 const config: CapacitorConfig = {
   appId: 'app.calendar.ethiopian.converter',
   appName: 'Ethiopian Calendar Converter',
-  webDir: 'dist',
+  webDir: '../dist',
   server: {
     androidScheme: 'https',
   },
